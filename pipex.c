dc7baaae1805bb7608700b2511c26f7ce16e6b48
kongrith <capuchino_cartoon@hotmail.com>
2024-11-02 01:03:57 +0700

    modify pipex.c

--- pipex.c
+++ pipex.c
@@ -23,44 +23,56 @@ int process1(t_data *data, char **argv, char **envp)
 	// debugging purpose
 	// dprintf(1, "%s\n", argv[2]);
 	// write(1, buf, 6);
-	char *args[3];
+	// char *args[3];
+	char **args;
+	args = ft_split(argv[2]);
 
-	args[0] = "grep";
-	args[1] = "a";
-	args[2] = NULL;
+	// args[0] = "grep";
+	// args[1] = "a";
+	// args[2] = NULL;
 	execve("/bin/grep", args, NULL);
 }
 
 int process2(t_data *data, char **argv, char **envp)
 {
+	int fd;
 	int nb_read;
 	int status;
 	char buf[BUFFER_SIZE + 1];
+	char **args;
 
-	waitpid(-1, &status, 0);
+	// waitpid(-1, &status, 0);
+	wait(NULL);
 	nb_read = -1;
 	close(data->pid[1]);
+	fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (fd == -1)
+		return (EXIT_FAILURE);
+	dup2(fd, 1);
+	close(fd);
 	dup2(data->pid[0], 0);
 	close(data->pid[0]);
 
+	args = ft_split(argv[3]);
+	execve("/bin/wc", args, NULL);
 	// debugging purpose
-	while (nb_read != 0)
-	{
-		nb_read = read(0, buf, BUFFER_SIZE);
-		if (nb_read == -1)
-		{
-			printf("Read error!\n");
-			return (1);
-		}
-		else if (nb_read == 0)
-		{
-			return (0);
-		}
+	// while (nb_read != 0)
+	// {
+	// 	nb_read = read(0, buf, BUFFER_SIZE);
+	// 	if (nb_read == -1)
+	// 	{
+	// 		printf("Read error!\n");
+	// 		return (1);
+	// 	}
+	// 	else if (nb_read == 0)
+	// 	{
+	// 		return (0);
+	// 	}
 
-		buf[nb_read] = '\0';
-		dprintf(1, "%s", buf);
-	}
-	dprintf(1, "%s\n", argv[4]);
+	// 	buf[nb_read] = '\0';
+	// 	dprintf(1, "%s", buf);
+	// }
+	// dprintf(2, "%s\n", argv[4]);
 	return (0);
 }
 
